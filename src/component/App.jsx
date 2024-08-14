70357f9de95e47d59654a5067274f00ec1344a55
ShaikbahirAkram <shaikbahirakram@gmail.com>
2024-08-14 20:03:06 +0530

    conditional React true /false //login form

---
+++ src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./login";
+
+
+const userdataInfo = true
+
+function App() {
+
+    return (
+        <Login userdataInfoProps={userdataInfo} />
+    );
+
+}
+
+export default App;
---
+++ src/component/login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Input from "./input";
+
+function Login(props) {
+    return (
+        <form>
+            <Input type="text" placeholder="username" />
+            <Input type="password" placeholder="password" />
+            {props.userdataInfoProps===true && <Input type="password" placeholder=" Confirm password" />}
+            {props.userdataInfoProps===true ?<button>Login</button>: <button>Register</button>}
+        </form>
+    );
+}
+
+export default Login;
