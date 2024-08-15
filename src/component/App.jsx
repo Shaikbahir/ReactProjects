06644b5a26733c6327af52acec705a9ffe0909e6
ShaikbahirAkram <shaikbahirakram@gmail.com>
2024-08-15 12:36:38 +0530

    hooks and state(upadted )

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,15 +1,47 @@
-import React from "react";
-import Login from "./login";
+// import React from "react";
+// import Login from "./login";
 
 
-const userdataInfo = true
+// const userdataInfo = true
+
+// function App() {
+
+//     return (
+//         <Login userdataInfoProps={userdataInfo} />
+//     );
+
+// }
+
+// export default App;//(for login form conditional statements)
+
+
+import React, { useState } from "react";
+
+var newDate = new Date()
+var now = newDate.getHours() + ":" + newDate.getMinutes() + ":" + newDate.getSeconds()
+
 
 function App() {
+    setInterval(getTime, 1000)
+    var [time, setTime] = useState(now)
+    function getTime() {
+        var date = new Date()
+        setTime(time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds())
+        console.log(time)
 
+    }
     return (
-        <Login userdataInfoProps={userdataInfo} />
-    );
+        <div>
+            <button onClick={getTime}>Time</button>
+            <h1>{time}</h1>
 
+        </div>
+
+    );
 }
 
+
+
+
 export default App;
+
